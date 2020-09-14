c3ec19b78f47c34e61ebe0c997bc19d77702011e
Roger <57421158+RogerCronin@users.noreply.github.com>
2020-09-14 12:18:35 -0400

    lime defend

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ client.on("ready", () => {
 
 client.on("message", message => {
   if(message.author.bot) return
-  if(message.content == "lime accuse") {
-    message.channel.send(stringReplacement(accusations[Math.floor(Math.random() * accusations.length)], message))
+  if(message.content == "lime accuse" || (message.content.startsWith("lime accuse") && message.mentions.members.size == 1 && message.content.split(" ").length == 3)) {
+    message.channel.send(stringReplacement(accusations[Math.floor(Math.random() * accusations.length)], message.mentions.members.first()))
+  } else if(message.content == "lime defend") {
+    message.channel.send(stringReplacement(defends[Math.floor(Math.random() * defends.length)], message))
   } else if(message.content == "lime invite") {
     let embed = new discord.MessageEmbed()
       .setColor(0x51ED39)
@@ -26,15 +28,23 @@ client.on("message", message => {
       .setTitle("Commands")
       .addFields(
         { name: "lime help", value: "Sends this help message." },
-        { name: "lime accuse", value: "Accuses a random member of the server." },
+        { name: "lime accuse [@username opt.]", value: "Accuses a random (or specific) member of the server." },
+        { name: "lime defend", value: "Defends themself or a random member of the server." },
         { name: "lime invite", value: "Sends an invite link to the bot." }
       )
     message.channel.send(embed)
   }
 })
 
-function stringReplacement(string, message) {
-  let players = message.guild.members.cache.random(2)
+function stringReplacement(string, input) {
+  if(input.permissions) {
+    let player2 = input.guild.members.cache.random()
+    while(player2.id == input.id) {
+      player2 = input.guild.members.cache.random()
+    }
+    return string.replace("%player%", input).replace("%player2%", player2)
+  }
+  let players = input.guild.members.cache.random(2)
   return string.replace("%player%", players[0]).replace("%player2%", players[1])
 }
 
@@ -51,7 +61,30 @@ const accusations = [
   "It's %player%",
   "%player% vented",
   "%player%'s lying vote them",
-  "I'm voting %player%"
+  "I'm voting %player%",
+  "I literally saw %player% vent, I swear if you don't vote him",
+  "It had to be %player% I didn't see them do a single task this entire game",
+  "%player% did a task but the bar didn't go up, kinda sus",
+  "%player% didn't do medbay scan right",
+  "I saw %player% standing in front of asteroids and guns never went off",
+  "It wasn't me %player% is lying i saw them kill vote them"
+]
+
+const defends = [
+  "It literally can't be me, I was in electrical the entire time",
+  "I saw %player% do garbage at the bottom, they're safe",
+  "%player% did shields and the lights turned on, they're good",
+  "I can confirm %player%",
+  "How was it me? I literally couldn't have killed him",
+  "Where was the body?",
+  "where",
+  "who",
+  "The lights were out, I couldn't see who killed who",
+  "Dude I was doing wires in admin",
+  "Stop calling meetings, I'm trying to do simon says",
+  "It's not %player%, they had so many chances to kill me but didn't",
+  "If you guys vote me you're actually throwing",
+  "WHAT THE FUCK I WAS NO WHERE NEAR THE FUCKING BODY STOP ACCUSING ME"
 ]
 
 client.login(fs.readFileSync("./token.txt", { encoding: "utf-8", flag: "r" }))
